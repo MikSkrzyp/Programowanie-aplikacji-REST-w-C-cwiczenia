2ec86a88f164574043e8f67a0a9aa6765616d1a5
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2024-02-19 09:12:34 +0100

    lekcja 1

---
+++ NetClassicWCFService/Service1/Service1.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Service1
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „Service1” w kodzie, usłudze i pliku konfiguracji.
+    // UWAGA: aby uruchomić klienta testowego WCF w celu przetestowania tej usługi, wybierz plik Service1.svc lub Service1.svc.cs w eksploratorze rozwiązań i rozpocznij debugowanie.
+    public class Service1 : IService1
+    {
+        public List<string> zwrocZadania(int value)
+        {
+            List<string> zadania = new List<string>()
+            {
+                "Zadanie 1 - przygotowac projekt zaliczeniowy",
+                "Zadanie 2",
+                "Zadanie 3"
+            };
+            return zadania;
+        }
+    }
+}
